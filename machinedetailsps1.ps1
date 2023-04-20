18719d096bcacefef3d32598f8f07cd2ef82c0fc
anamika242 <achamola@estee.com>
2023-04-20 09:16:03 +0530

    uploading sample script

---
+++ machinedetailsps1.ps1
@@ -0,0 +1,14 @@
+﻿$path= "C:\temp\ServerDetails.txt"
+$output= "Details of the machine name, domain, total physical memory"
+Add-Content -path $path -Value $output 
+[string]$detail= Get-WmiObject -Class Win32_ComputerSystem | Select-Object Name, Domain, TotalPhysicalMemory 
+Add-Content $path $detail
+$output= "CPU details: "
+Add-Content $path $output
+[string]$detail= Get-WmiObject -Class Win32_Processor | Select-Object Name, NumberOfLogicalProcessors
+Add-Content $path $detail
+
+$output= "Disk space details: "
+Add-Content $path $output
+[string]$detail= Get-WmiObject -Class Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 } | Select-Object DeviceID, VolumeName, Size
+Add-Content $path $detail
